494c11809d93d11a4d6f2d7f762b50e0656b6fd5
KpSantiago <santiago.kauajose11@gmail.com>
2024-11-18 15:46:53 -0300

    fix: make create gym command props requireds

--- GymPass.Application/CQRs/Commands/Requests/CreateGymCommand.cs
+++ GymPass.Application/CQRs/Commands/Requests/CreateGymCommand.cs
@@ -1,16 +1,21 @@
 using GymPass.Application.CQRs.Commands.Responses;
 using GymPass.Domain.ValueObjects;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymPass.Application.CQRs.Commands.Requests;
 
 public class CreateGymCommand : IRequest<CreateGymResponse>
 {
+    [Required]
     public string Title { get; set; } = default!;
 
+    [Required]
     public string Description { get; set; } = default!;
 
+    [Required]
     public string Phone { get; set; } = default!;
 
+    [Required]
     public Cordinate Cordinate { get; set; } = default!;
 }
